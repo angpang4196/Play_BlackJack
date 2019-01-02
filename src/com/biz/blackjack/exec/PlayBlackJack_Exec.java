c3e5c8aa549e31080fbd1ac76e70077fc729b01c
chodonghyuck <colplayer@naver.com>
2019-01-02 17:20:47 +0900

    update

---
+++ src/com/biz/blackjack/exec/PlayBlackJack_Exec.java
@@ -0,0 +1,124 @@
+package com.biz.blackjack.exec;
+
+import java.util.Scanner;
+
+import com.biz.blackjack.service.MakeCardService;
+
+public class PlayBlackJack_Exec {
+
+	/*
+	 * 모든 클래스의 진입점 method
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		/*
+		 * MakeCardService를 이용한 mcs객체 선언 및 초기화
+		 */
+		MakeCardService mcs = new MakeCardService();
+
+		mcs.gameStart(); // 게임 시작을 console에 표시
+
+		mcs.makeNum(); // 카드의 숫자 생성
+		mcs.makePat(); // 카드의 모양 생성
+		mcs.makeAlp(); // 카드의 알파벳 생성(A, K, Q, J)
+
+		mcs.makeNumCard(); // 모양과 숫자를 조합한 카드 생성 (36개)
+		mcs.makeAlpCard(); // 모양과 알파벳을 조합한 카드 생성 (16개)
+
+		mcs.randomCard(); // 카드 섞기
+		mcs.addCard(); // 각 리스트에 카드 저장하기
+
+		mcs.showDealer1(); // 딜러의 오픈된 한 장의 정보와 플레이어가 받은 두 장의 정보와 점수를 console에 출력하는 method
+
+		/*
+		 * 키보드로부터 입력받기 위한 Scanner 선언 및 초기화
+		 */
+		Scanner scan = new Scanner(System.in);
+
+		/*
+		 * MakeCardService 클래스의 getPCard()에 매개변수로 인덱스 값을 전달하기위해 선언 및 2로 저장 2로 저장한 이유는
+		 * 0번째와 1번째는 이미 초기에 저장이 되어있기 때문에 2번째부터 저장하기위해
+		 */
+		int intIndex = 2;
+
+		/*
+		 * while문을 이용해 플레이어의 선택(멈출지, 추가로 카드를 얻을지)
+		 */
+		while (true) {
+
+			/*
+			 * console에 선택란을 보여주는 코드
+			 */
+			System.out.println("0 : STOP , 1 : GET");
+			System.out.print("선택 >>>");
+
+			/*
+			 * console에 표시된 위의 내용을 보고 입력을 받는 코드 키보드로 부터 입력받은 문자열을 strWrite라는 문자열형 변수에 저장
+			 */
+			String strWrite = scan.nextLine();
+
+			/*
+			 * 0과 1중 입력을 받았으니 밑의 if문에서 비교하기위해 int형으로 변환 후 intWrite라는 변수에 저장
+			 */
+			int intWrite = Integer.valueOf(strWrite);
+
+			/*
+			 * if조건문을 이용해 0이면 스탑하고 딜러와 플레이어의 점수를 비교해 승패 결정
+			 */
+			if (intWrite == 0) {
+
+				/*
+				 * console에 stop을 선택했다는 메세지를 표시
+				 */
+				System.out.println("STOP을 선택하셨습니다.");
+
+				/*
+				 * 딜러의 오픈되지않은 카드의 정보와 점수를 알려주는 method
+				 */
+				mcs.showDealerAno();
+
+				/*
+				 * 딜러의 점수리스트와 플레이어의 점수리스트의 각 점수를 합쳐서 승패 정하는 method
+				 */
+				mcs.exam();
+
+				/*
+				 * 게임 종료
+				 */
+				break;
+			}
+			if (intWrite == 1) {
+
+				/*
+				 * 위에서 intIndex의 값을 2로 저장 후 MakeCardService 객체 mcs의 getPCard()에 매개변수로 인덱스값을 전달하고
+				 * return받은 1을 intIndex에 더해주고 다시 반복하는 코드 즉 이 말은 초기값인 2부터 1씩 intIndex가 증가한다는 말이
+				 * 된다.
+				 */
+
+				/*
+				 * 플레이어가 얻고싶으면 실행되는 method에 인덱스 값을 매개변수로 전달해주고 리턴받은 값을 int형 변수 intScore에 저장
+				 */
+				int intScore = mcs.getPCard(intIndex);
+
+				/*
+				 * 위의 return받은 값이 0이라는 말은 service 클래스에 가보면 알겠지만 플레이어가 얻은 카드까지 점수를 합해서 점수가 21점이
+				 * 초과했을 때 0을 return하게 했다. 그래서 그 return받은 값이 0 이면 바로 딜러가 이긴 결과이기 때문에 break를
+				 * 걸어주었다.
+				 */
+				if (intScore == 0) {
+					break;
+				}
+
+				/*
+				 * return받은 값이 0이 아니라면 점수가 21점이 아직 안 넘었다는 말이다. 그리고 return받은 값은 1이되는데 원래
+				 * intIndex에 + 1 을 해서 다음 순서의 카드를 받기위해 아래 코드를 적었고, while문 처음으로 가서 다시 카드를 얻을지 그만
+				 * 받을지 선택하게 한다.
+				 */
+				intIndex += intScore;
+
+			}
+		}
+	}
+
+}
---
+++ src/com/biz/blackjack/service/MakeCardService.java
@@ -0,0 +1,450 @@
+package com.biz.blackjack.service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.Scanner;
+
+public class MakeCardService {
+
+	List<String> numList; // 숫자 9개를 담을 리스트 선언
+	List<String> patList; // 모양 4개를 담을 리스트 선언
+	List<String> alpList; // A, K, Q, J 담을 리스트 선언
+	List<String> cardList; // 카드 52장을 담을 리스트 선언
+
+	List<String> dealerCList; // 딜러의 카드를 담을 리스트 선언
+	List<String> playerCList; // 플레이어의 카드를 담을 리스트 선언
+	List<Integer> dScoreList; // 딜러가 받은 카드 한 장씩의 점수를 담을 리스트 선언
+	List<Integer> pScoreList; // 플레이어가 받은 카드 한 장씩의 점수를 담을 리스트 선언
+
+	/*
+	 * 생성자에서 String형 리스트 6개와 Integer형 리스트2개를 초기화
+	 */
+	public MakeCardService() {
+		numList = new ArrayList();
+		patList = new ArrayList();
+		alpList = new ArrayList();
+		cardList = new ArrayList();
+
+		dealerCList = new ArrayList();
+		playerCList = new ArrayList();
+		dScoreList = new ArrayList();
+		pScoreList = new ArrayList();
+	}
+
+	/*
+	 * 카드의 숫자 (2 ~ 10 )를 생성해서 String형 리스트 numList에 저장하는 method
+	 */
+	public void makeNum() {
+		/*
+		 * 반복문을 이용해서 2 ~ 10 숫자를 numList에 문자열 값으로 저장
+		 */
+		for (int i = 0; i < 9; i++) {
+			numList.add((i + 2) + "");
+		}
+	}
+
+	/*
+	 * 카드의 하트, 다이아, 스페이드, 클로버 모양을 String형 리스트 patList에 저장하는 method
+	 */
+	public void makePat() {
+		/*
+		 * String형 리스트 patList에 모양 4개를 저장
+		 */
+		patList.add("HEART");
+		patList.add("SPADE");
+		patList.add("DIAMOND");
+		patList.add("CLOVER");
+	}
+
+	/*
+	 * 알파벳 A, K, Q, J를 String형 리스트 alpList에 저장하는 method
+	 */
+	public void makeAlp() {
+		alpList.add("A");
+		alpList.add("K");
+		alpList.add("Q");
+		alpList.add("J");
+	}
+
+	/*
+	 * 모양과 숫자을 조합해서 전체 카드를 만들어내는 method
+	 */
+	public void makeNumCard() {
+		/*
+		 * 이중 for문을 이용하여
+		 */
+		for (String t : numList) {
+			for (String s : patList) {
+				/*
+				 * patList에 있는 모양과 numList에 있는 숫자를 조합하여 String형 변수 strNumCard에 저장
+				 */
+				String strNumCard = s + " " + t;
+				/*
+				 * 위에서 만든 String형 변수 strNumCard 들을 cardList에 저장
+				 */
+				cardList.add(strNumCard);
+			}
+		}
+	}
+
+	/*
+	 * 모양과 알파벳을 조합하여 카드를 만드는 method
+	 */
+	public void makeAlpCard() {
+		/*
+		 * 이중 for문을 이용해서 모양과 숫자를 String형 변수 strAlpCard에 담고 그 값을 cardList에 저장
+		 */
+		for (String s : patList) {
+			for (String t : alpList) {
+				String strAlpCard = s + " " + t;
+				cardList.add(strAlpCard);
+			}
+		}
+	}
+
+	/*
+	 * 카드 섞는 method
+	 */
+	public void randomCard() {
+		/*
+		 * for문을 이용해 cardList에 담긴 52장의 카드들을 10번 섞기
+		 */
+		for (int i = 0; i < 10; i++) {
+			Collections.shuffle(cardList);
+		}
+	}
+
+	/*
+	 * 번갈아가면서 딜러와 플레이어의 카드리스트에 카드를 저장하는 method
+	 */
+	public void addCard() {
+		/*
+		 * 리스트의 사이즈를 int형 변수에 저장
+		 */
+		int intSize = cardList.size();
+		/*
+		 * for문을 이용해서 번갈아가면서 짝수번째는 딜러카드로, 홀수번째는 플레이어카드로 저장
+		 */
+		for (int i = 0; i < intSize; i++) {
+			if (i % 2 == 0) {
+				dealerCList.add(cardList.get(i));
+			}
+			if (i % 2 != 0) {
+				playerCList.add(cardList.get(i));
+			}
+		}
+	}
+
+	/*
+	 * console에 게임시작을 알려주는 method
+	 */
+	public void gameStart() {
+
+		System.out.println("==================================================================");
+		System.out.println("1 대 1 블랙잭 게임 시작");
+		System.out.println("------------------------------------------------------------------");
+
+	}
+
+	/*
+	 * 딜러의 오픈된 한 장의 정보와 플레이어가 받은 두 장의 정보와 점수를 console에 출력하는 method
+	 */
+	public void showDealer1() {
+
+		/*
+		 * 딜러의 오픈된 한 장의 정보를 console에 표시
+		 */
+		System.out.println("딜러의 카드 한 장은 " + dealerCList.get(0) + "입니다.");
+
+		/*
+		 * 플레이어가 받은 두 장의 정보를 console에 표시
+		 */
+		System.out.println("플레이어가 받은 카드 두 장은 " + playerCList.get(0) + ", " + playerCList.get(1) + "입니다.");
+
+		/*
+		 * 플레이어가 처음에 받은 두장의 정보를 makeScore()의 매개변수로 보내서 리턴받은 값(점수)를 int형 변수 intPFScore에
+		 * 저장
+		 */
+		int intPFScore = makeScore(playerCList.get(0)) + makeScore(playerCList.get(1));
+
+		/*
+		 * intPFScore에 있는 점수를 플레이어 점수리스트인 pScoreList에 저장
+		 */
+		pScoreList.add(intPFScore);
+
+		/*
+		 * 플레이어가 받은 두 장의 점수를 console에 표시
+		 */
+		System.out.println("현재 플레이어의 점수는 " + intPFScore + "입니다.");
+	}
+
+	/*
+	 * 딜러의 오픈되지않은 카드의 정보와 점수를 알려주는 method
+	 */
+	public void showDealerAno() {
+
+		/*
+		 * 딜러의 오픈되지않은 카드의 정보를 오픈하면서 console에 표시
+		 */
+		System.out.println("딜러의 다른 한 장은 " + dealerCList.get(1) + "입니다.");
+
+		/*
+		 * 딜러의 첫 두 장의 점수를 구하는 부분 첫번째 카드와 두번째카드의 정보를 makeScore()에 매개변수로 전달해서 리턴받은 값(점수)를
+		 * int형 변수 intDFScore에 저장
+		 */
+		int intDFScore = makeScore(dealerCList.get(0)) + makeScore(dealerCList.get(1));
+
+		/*
+		 * intDFScore에 담긴 값(점수)를 console에 표시
+		 */
+		System.out.println("딜러의 카드 두 장의 점수는 " + intDFScore + "점입니다.");
+
+		/*
+		 * 그 점수값을 딜러의 점수리스트 dScoreList에 저장
+		 */
+		dScoreList.add(intDFScore);
+
+		/*
+		 * 반복문을 이용해서 딜러의 첫번째카드와 두번째카드의 점수합이 16점이하일 때 카드 한 장씩을 더하고 16과 비교 초기값을 2로 준이유는 0과
+		 * 1은 위의 method에서 알고있어서
+		 */
+		for (int i = 2; i < 15; i++) {
+
+			/*
+			 * 딜러의 첫 카드와 두번째 카드의 점수합이 16이하이면 >>> if조건문 사용
+			 */
+			if (intDFScore <= 16) {
+
+				/*
+				 * 점수합이 16점이하라면 한 장을 더 받는다는 메세지와 그 받은 카드의 정보를 console에 표시
+				 */
+				System.out.println("딜러의 점수가 16점 이하이므로 카드 한 장을 더 받습니다.");
+				System.out.println("딜러가 추가로 받은 카드는 " + dealerCList.get(i) + "입니다.");
+
+				/*
+				 * 추가로 받은 카드의 점수를 makeScore()에 매개변수로 전달하여 return받은 값을 intDFScore에 추가 저장
+				 */
+				intDFScore += makeScore(dealerCList.get(i));
+
+				/*
+				 * 그 추가로 받은 카드점수를 초기 점수와 합산하여 console에 표시
+				 */
+				System.out.println("딜러의  점수는 " + intDFScore + "점입니다.");
+
+				/*
+				 * dScoreList에는 딜러의 첫 카드와 두번째 카드 점수는 이미 저장이 되어있다. 그래서 추가로 받은 카드의 점수만 추가시켜주면 된다.
+				 * 추가로 받은 카드의 점수는 makeScore에 추가로 받은 카드의 정보를 매개변수로 전달하여 return받은 값이다.
+				 */
+				dScoreList.add(makeScore(dealerCList.get(i)));
+			}
+		}
+	}
+
+	/*
+	 * main method에서 추가로 카드를 받고싶으면 실행되는 method 매개변수 intIndex는 main에서 전달
+	 */
+	public int getPCard(int intIndex) {
+
+		/*
+		 * 카드를 한 장얻고나면 다음 순서의 카드를 얻기위해 for문을 이용 main에서 전달받은 intIndex는 2부터 1씩 증가
+		 */
+		for (int i = intIndex; i < 15; i++) {
+
+			/*
+			 * console에 플레이어가 한 장의 카드를 얻는다는 메세지와 얻은 카드의 정보를 표시
+			 */
+			System.out.println("플레이어는 한 장의 카드를 더 얻습니다.");
+			System.out.println("얻은 카드는 " + playerCList.get(i) + "입니다.");
+
+			/*
+			 * int형 변수 intPlayerScore에 플레이어가 처음에 받은 두 장의 카드의 점수를 합하여 저장 makeScore()에 매개변수로 두
+			 * 장의 카드의 정보를 전달
+			 */
+			int intPlayerScore = makeScore(playerCList.get(0)) + makeScore(playerCList.get(1));
+
+			/*
+			 * 처음 받은 두장의 점수에 추가로 받은 카드의 점수를 더해주는 코드 마찬가지로 makeScore에 플레이어가 추가로 받은 카드 정보를
+			 * 매개변수로 전달해주고 return받은 값을 더해줌
+			 */
+			intPlayerScore += makeScore(playerCList.get(i));
+
+			/*
+			 * return받은 값(점수)를 플레이어의 점수리스트에 추가
+			 */
+			pScoreList.add(makeScore(playerCList.get(i)));
+
+			/*
+			 * 추가로 받은 카드의 점수까지 합했을 때 21이 넘으면 자동으로 딜러가 이기는 결과가 된다.
+			 */
+			if (intPlayerScore > 21) {
+				System.out.println("21점을 초과했으므로 딜러 승");
+
+				/*
+				 * 21점초과가 되면 그 21점 초과가 된 점수를 overScore() method의 매개변수로 전달해주고 return받은 0을 
+				 * main method로 다시 return해 주게 된다.
+				 */
+				return this.overScore(intPlayerScore);
+			}
+
+			/*
+			 * 21점이 초과가 되지 않았다면 1을 이 method를 호출한 main method로 return하게 되는데 return 값을 1로 설정한 이유는
+			 * 인덱스를 1씩 증가시키기위해 return해 주는 값을 1로 설정하였다.
+			 */
+			return 1;
+		}
+		return 1;
+	}
+	
+	/*
+	 * overScore() method가 getPCard() method에서 전달한 매개변수를 받고 0을 return해 주는 method
+	 * 설명 : getPCard() method에서 플레이어가 얻은 카드의 점수까지 합해서 21점을 넘었을 때 이 method로 그 점수를 
+	 * 	      매개변수로 보내주게 된다. 
+	 */
+	public int overScore(int intOverScore) {
+		return 0;
+	}
+
+	/*
+	 * 카드 정보를 매개변수로 받아서 빈 칸을 기준으로 분해하고 뒷 글자를 점수계산하는 method의 매개변수로 전달하는 method
+	 */
+	public int makeScore(String strCard) {
+		/*
+		 * 빈 칸을 기준으로 분해하고 각각 strSp라는 문자열 배열에 담는다.
+		 */
+		String[] strSp = strCard.split(" ");
+		/*
+		 * 뒷 글자가 점수를 구하는 데 필요하니까 배열의 1번째 값을 문자열 변수에 담고 점수계산하는 method의 매개변수로 전달
+		 */
+		String strScoreCode = strSp[1];
+		int intScore = scoreCode(strScoreCode);
+		/*
+		 * 점수계산해서 리턴받아온 값을 int형 변수에 담고 makeScore() 호출한 곳으로 리턴해줌.
+		 */
+		return intScore;
+	}
+
+	/*
+	 * 카드 정보중 뒷 글자를 매개변수로 받아서 점수값을 리턴해주는 method
+	 */
+	public int scoreCode(String strCode) {
+
+		// 뒷 글자가 A일 경우 1점을 리턴
+		if (strCode.equals("A")) {
+			return 1;
+		}
+		// 뒷 글자가 J일 경우 10점을 리턴
+		if (strCode.equals("J")) {
+			return 10;
+		}
+		// 뒷 글자가 Q일 경우 10점을 리턴
+		if (strCode.equals("Q")) {
+			return 10;
+		}
+		// 뒷 글자가 K일 경우 10점을 리턴
+		if (strCode.equals("K")) {
+			return 10;
+		}
+		/*
+		 * 위 4가지경우를 제외하면 나머지 뒷 글자는 모두 int형변환이 가능하다. 그래서 형변환을 한 후에 그 변환시킨 값을 그대로 점수값으로
+		 * 리턴해줌
+		 */
+		int intScore = Integer.valueOf(strCode);
+		return intScore;
+	}
+
+	/*
+	 * 딜러의 점수리스트와 플레이어의 점수리스트의 각 점수를 합쳐서 승패 정하는 method
+	 */
+	public void exam() {
+
+		/*
+		 * 두 사람의 점수 합을 구하기위해 intD, indP라는 int형 변수 선언 및 초기화
+		 */
+		int intD = 0;
+		int intP = 0;
+
+		/*
+		 * dScoreList(딜러 점수리스트)에 있는 딜러가 가지고있는 각 카드의 점수를 다 더하는 코드
+		 */
+		for (int d : dScoreList) {
+			intD += d;
+		}
+
+		/*
+		 * pScoreList(플레이어 점수리스트)에 있는 딜러가 가지고있는 각 카드의 점수를 다 더하는 코드
+		 */
+		for (int p : pScoreList) {
+			intP += p;
+		}
+
+		/*
+		 * console에 두 사람의 점수를 보여주는 코드
+		 */
+		System.out.println("------------------------------------------------------------------");
+		System.out.println("딜러 : " + intD + "점");
+		System.out.println("플레이어 : " + intP + "점");
+		System.out.println("==================================================================");
+		/*
+		 * intDS는 21점과 딜러점수의 점수 차이 , intPS는 21점과 플레이어의 점수 차이
+		 */
+		int intDGap = intD - 21;
+		int intPGap = intP - 21;
+
+		while (true) {
+
+			// 딜러와 플레이어 점수 모두 21점을 초과했을 때
+			if (intDGap > 0 && intPGap > 0) {
+				System.out.println("결과 : 딜러 승");
+				break;
+			}
+			// 딜러점수가 21을 초과했을 때
+			if (intDGap > 0) {
+				System.out.println("결과 : 플레이어 승");
+				break;
+			}
+
+			// 플레이어 점수가 21을 초과했을 때
+			if (intPGap > 0) {
+				System.out.println("결과 : 딜러 승");
+				break;
+			}
+
+			// 딜러점수가 21일 때
+			if (intDGap == 0) {
+				System.out.println("결과 : 딜러 승");
+				break;
+			}
+
+			// 플레이어점수가 21점일 때
+			if (intPGap == 0) {
+				System.out.println("Black Jack !!!");
+				System.out.println("플레이어 승");
+				break;
+			}
+
+			// 딜러와 플레이어 점수가 같을 때
+			if (intDGap == intPGap) {
+				System.out.println("결과 : DRAW");
+				break;
+			}
+
+			// 딜러와 플레이어 점수 모두 21점보다 작을 때
+			if (intDGap < 0 && intPGap < 0) {
+
+				// 딜러의 점수가 21점보다 더 가까울 때
+				if (intDGap > intPGap) {
+					System.out.println("결과 : 딜러 승");
+					break;
+				}
+
+				// 플레이어의 점수가 21점보다 더 가까울 때
+				if (intDGap < intPGap) {
+					System.out.println("결과 : 플레이어 승");
+					break;
+				}
+			}
+		}
+	}
+
+}
